c91643c3d08cec56324f9273ba6ea1ca83f27942
Dennis-Zhao1 <zdq198654@gmail.com>
2025-09-26 12:10:49 -0400

    deploy green

--- blue.tf
+++ blue.tf
@@ -18,10 +18,10 @@ resource "aws_instance" "blue" {
 }
 
 resource "aws_lb_target_group" "blue" {
-  name     = "blue-tg-${random_pet.app.id}-lb"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  name_prefix = "bl-"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
 
   health_check {
     port     = 80
